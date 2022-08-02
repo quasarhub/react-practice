dc64aa5eb215f6a66910d6ffc86ef3ecc5bbdf98
Youngjin Jo <71948604+quasarhub@users.noreply.github.com>
2022-08-02 12:27:36 +0900

    Initial Setup

--- src/App.js
+++ src/App.js
@@ -1,25 +1,19 @@
-import logo from './logo.svg';
+// class형 component를 이용해보자.
+// 그러려면 class를 component로 바꿔야하는데,
+// React가 가지고 있는 Component를 상속받아서 사용해야 한다.
+import React, { Component } from 'react';
+
 import './App.css';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+// 외부에서 사용이 되려면 반드시 export 되어야 한다.
+// 그래야 class를 사용할 수 있다.
+export default class App extends Component {
+  // render라는 메서드는 화면을 그려준다.
+  render() {
+    return (
+      <div className="container">
+        <div className="todoBlock">오늘의 할 일</div>
+      </div>
+    );
+  }
 }
-
-export default App;
